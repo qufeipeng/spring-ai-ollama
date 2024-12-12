b448c19be0bdd2dab41839f25192288e467171c6
qufeipeng@126.com <flink@2022>
2024-12-12 17:00:18 +0800

    add

--- src/main/java/org/example/ChatController.java
+++ src/main/java/org/example/ChatController.java
@@ -2,9 +2,6 @@ package org.example;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +11,16 @@ import java.util.Map;
 
 @RestController
 public class ChatController {
-
     private final ChatClient chatClient;
-    @Value("classpath:your-prompt-template.st")
-    Resource promptTemplateResource;
+
     public ChatController(ChatClient.Builder builder) {
         this.chatClient = builder.build();
     }
 
     @GetMapping("/ai/generateStream")
     public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        //Prompt prompt = new Prompt(new UserMessage(message));
-        //return chatClient.stream(prompt);
         System.out.println("message: " + message);
-        PromptTemplate promptTemplate = new PromptTemplate(promptTemplateResource);
-        Prompt prompt = promptTemplate.create(Map.of("message", message));
+        Prompt prompt = new Prompt(message);
         return chatClient.prompt(prompt).stream().content();
     }
 }
